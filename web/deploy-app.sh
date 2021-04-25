f898123e58b457c7212910f7730830ce6ff19863
root <root@images.asia-southeast1-b.c.avian-cat-302217.internal>
2021-04-25 20:15:03 +0000

    Local envoy

--- web/deploy-app.sh
+++ web/deploy-app.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set TOMCAT_HOME=../apache-tomcat-9.0.45
 rm -rf ./dist/
+
 npx webpack --mode development
 #npx webpack --mode development ./client.js
+
 mkdir -p $TOMCAT_HOME/webapps/client/dist/
 cp dist/main.js $TOMCAT_HOME/webapps/client/dist/
 cp page.js $TOMCAT_HOME/webapps/client/
