d65c1899b97ce57b581d4150eb1d0d36ebaff54c
root <root@grpc.asia-southeast1-b.c.avian-cat-302217.internal>
2021-04-26 20:46:45 +0000

    Fix setup issue

--- envoy/setup.sh
+++ envoy/setup.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 #https://www.envoyproxy.io/docs/envoy/latest/start/install#install-envoy-on-ubuntu-linux
-sudo apt-get update
-sudo apt-get install -y \
-apt-transport-https \
-ca-certificates \
-curl \
-gnupg-agent \
-software-properties-common
-
+apt update  --fix-missing
+home_url=$(sudo cat /etc/*release | grep HOME_URL | cut -d'"' -f2)
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+if [ "$home_url" = "https://www.debian.org/" ]; then
+  sudo apt install -y gnupg2
+else
+  sudo apt install -y gnupg-agent
+fi
 curl -sL 'https://getenvoy.io/gpg' | sudo apt-key add -
-
-apt-key fingerprint 6FF974DB
-
-sudo add-apt-repository \
-"deb [arch=amd64] https://dl.bintray.com/tetrate/getenvoy-deb \
-$(lsb_release -cs) \
-stable"
-sudo apt-get update && sudo apt-get install -y getenvoy-envoy
+# verify the key
+apt-key fingerprint 6FF974DB | grep "5270 CEAC"
+sudo add-apt-repository "deb [arch=amd64] https://dl.bintray.com/tetrate/getenvoy-deb $(lsb_release -cs) stable"
+apt update
+sudo apt install -y getenvoy-envoy
 
 envoy --version
 echo -e "\e[1;32mInstalled envoy proxy\e[0m"
