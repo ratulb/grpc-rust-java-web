96453bd3ed87f6e3c1b71cae71703745d9068f21
root <root@grpc.asia-southeast1-b.c.avian-cat-302217.internal>
2021-04-27 16:54:30 +0000

    Formatting

--- envoy/image-run.sh
+++ envoy/image-run.sh
@@ -1,5 +1,5 @@
-docker stop envoy-grpc-proxy 2> /dev/null
-docker rm envoy-grpc-proxy 2> /dev/null
-docker run  --name envoy-grpc-proxy -p 9901:9901 \
-	-p 30030:10000 -v $PWD/envoy.yaml:/etc/envoy/envoy.yaml \
-	envoyproxy/envoy-alpine:v1.18-latest
+docker stop envoy-grpc-proxy 2>/dev/null
+docker rm envoy-grpc-proxy 2>/dev/null
+docker run --name envoy-grpc-proxy -p 9901:9901 \
+  -p 10000:10000 -v $PWD/envoy.yaml:/etc/envoy/envoy.yaml \
+  envoyproxy/envoy-alpine:v1.18-latest
--- envoy/setup.sh
+++ envoy/setup.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 #https://www.envoyproxy.io/docs/envoy/latest/start/install#install-envoy-on-ubuntu-linux
-apt update  --fix-missing
-home_url=$(sudo cat /etc/*release | grep HOME_URL | cut -d'"' -f2)
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-if [ "$home_url" = "https://www.debian.org/" ]; then
-  sudo apt install -y gnupg2
+if ! command envoy --version; then
+  apt update --fix-missing
+  home_url=$(sudo cat /etc/*release | grep HOME_URL | cut -d'"' -f2)
+  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+  if [ "$home_url" = "https://www.debian.org/" ]; then
+    sudo apt install -y gnupg2
+  else
+    sudo apt install -y gnupg-agent
+  fi
+  curl -sL 'https://getenvoy.io/gpg' | sudo apt-key add -
+  # verify the key
+  apt-key fingerprint 6FF974DB | grep "5270 CEAC"
+  sudo add-apt-repository "deb [arch=amd64] https://dl.bintray.com/tetrate/getenvoy-deb $(lsb_release -cs) stable"
+  apt update --fix-missing
+  sudo apt install -y getenvoy-envoy
+  echo -e "\e[1;32mInstalled envoy proxy\e[0m"
+  envoy --version
 else
-  sudo apt install -y gnupg-agent
+  echo -e "\e[1;32menvoy proxy is already installed\e[0m"
 fi
-curl -sL 'https://getenvoy.io/gpg' | sudo apt-key add -
-# verify the key
-apt-key fingerprint 6FF974DB | grep "5270 CEAC"
-sudo add-apt-repository "deb [arch=amd64] https://dl.bintray.com/tetrate/getenvoy-deb $(lsb_release -cs) stable"
-apt update --fix-missing
-sudo apt install -y getenvoy-envoy
 
-envoy --version
-echo -e "\e[1;32mInstalled envoy proxy\e[0m"
