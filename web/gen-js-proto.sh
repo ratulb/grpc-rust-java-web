be841bfcacaa6335fff416bfb0cdf2828f5b0dc5
root <root@test.asia-southeast1-b.c.avian-cat-302217.internal>
2021-05-02 09:01:18 +0000

    WIP

--- web/gen-js-proto.sh
+++ web/gen-js-proto.sh
@@ -1,2 +1,2 @@
-protoc ./proto/echo.proto --js_out=import_style=commonjs,binary:./ --grpc-web_out=import_style=commonjs,mode=grpcwebtext:.
+protoc --proto_path= ./proto/echo.proto --js_out=import_style=commonjs,binary:./ --grpc-web_out=import_style=commonjs,mode=grpcwebtext:.
 mv proto/*.js .
